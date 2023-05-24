b7d7761ab816065692fd92a999fe2c4d8359d5c6
Ilemona <ilemona.philip@gmail.com>
2023-05-24 13:31:00 -0700

    alphabet in small cases

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - a code that prints the alphabet in lowercase
+ * Return: Void.
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+}
